25c4f05aa293afce34af68278c8d9803ec03f9a2
Nathanael Mario Duarte Delfino <nathanael.delfino@live.com>
2022-08-29 14:11:24 -0300

    :up: First commit

---
+++ Settings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinimalApiAuth
+{
+    public static class Settings
+    {
+        public static string Sectret { get; private set; } = "1af22eb4cd8c4d3cad97a98461bd5bc0";
+    }
+}
